43f0613ad9774f27c9bf27701ab886be7ce5058a
Simon Guest <simon.guest@agresearch.co.nz>
2023-05-16 17:27:30 +1200

    Add --exclude-rootdir-metadata option

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,35 @@ struct Opt {
     /// Directories to hash
     #[arg(value_name = "DIR")]
     dirs: Vec<PathBuf>,
+
+    /// Whether to exclude the root directory metadata from the checksum
+    #[arg(long)]
+    exclude_rootdir_metadata: bool,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Optional {
+    Some,
+    None,
+}
+
+impl Optional {
+    fn apply<T>(&self, item: T) -> Option<T> {
+        match self {
+            Self::Some => Some(item),
+            Self::None => None,
+        }
+    }
+}
+
+impl From<bool> for Optional {
+    fn from(value: bool) -> Self {
+        if value {
+            Self::Some
+        } else {
+            Self::None
+        }
+    }
 }
 
 fn main() {
@@ -63,8 +92,10 @@ fn main() {
 
     configure_thread_pool(&opt);
 
+    let metadata_optional = Optional::from(!opt.exclude_rootdir_metadata);
+
     if opt.dirs.is_empty() {
-        let checksum = checksum_current_dir();
+        let checksum = checksum_current_dir(metadata_optional);
         let _ = writeln!(io::stdout(), "{}", checksum);
         return;
     }
@@ -75,7 +106,7 @@ fn main() {
         if let Err(error) = env::set_current_dir(canonical) {
             die(label, error);
         }
-        let checksum = checksum_current_dir();
+        let checksum = checksum_current_dir(metadata_optional);
         let _ = writeln!(io::stdout(), "{}  {}", checksum, label.display());
     }
 }
@@ -130,13 +161,18 @@ impl Checksum {
     }
 }
 
-fn checksum_current_dir() -> Checksum {
+fn checksum_current_dir(metadata_optional: Optional) -> Checksum {
     let checksum = Checksum::new();
-    rayon::scope(|scope| entry(scope, &checksum, Path::new(".")));
+    rayon::scope(|scope| entry(scope, &checksum, Path::new("."), metadata_optional));
     checksum
 }
 
-fn entry<'scope>(scope: &Scope<'scope>, checksum: &'scope Checksum, path: &Path) {
+fn entry<'scope>(
+    scope: &Scope<'scope>,
+    checksum: &'scope Checksum,
+    path: &Path,
+    metadata_optional: Optional,
+) {
     let metadata = match path.symlink_metadata() {
         Ok(metadata) => metadata,
         Err(error) => die(path, error),
@@ -148,7 +184,7 @@ fn entry<'scope>(scope: &Scope<'scope>, checksum: &'scope Checksum, path: &Path)
     } else if file_type.is_symlink() {
         symlink(checksum, path, metadata)
     } else if file_type.is_dir() {
-        dir(scope, checksum, path, metadata)
+        dir(scope, checksum, path, metadata_optional.apply(metadata))
     } else if file_type.is_socket() {
         socket(checksum, path, metadata)
     } else {
@@ -161,7 +197,7 @@ fn entry<'scope>(scope: &Scope<'scope>, checksum: &'scope Checksum, path: &Path)
 }
 
 fn file(checksum: &Checksum, path: &Path, metadata: Metadata) -> Result<()> {
-    let mut sha = begin(path, &metadata, b'f');
+    let mut sha = begin(path, Some(&metadata), b'f');
 
     // Enforced by memmap: "memory map must have a non-zero length"
     if metadata.len() > 0 {
@@ -176,7 +212,7 @@ fn file(checksum: &Checksum, path: &Path, metadata: Metadata) -> Result<()> {
 }
 
 fn symlink(checksum: &Checksum, path: &Path, metadata: Metadata) -> Result<()> {
-    let mut sha = begin(path, &metadata, b'l');
+    let mut sha = begin(path, Some(&metadata), b'l');
     sha.update(path.read_link()?.as_os_str().as_bytes());
     checksum.put(sha);
 
@@ -187,33 +223,35 @@ fn dir<'scope>(
     scope: &Scope<'scope>,
     checksum: &'scope Checksum,
     path: &Path,
-    metadata: Metadata,
+    metadata: Option<Metadata>,
 ) -> Result<()> {
-    let sha = begin(path, &metadata, b'd');
+    let sha = begin(path, metadata.as_ref(), b'd');
     checksum.put(sha);
 
     for child in path.read_dir()? {
         let child = child?.path();
-        scope.spawn(move |scope| entry(scope, checksum, &child));
+        scope.spawn(move |scope| entry(scope, checksum, &child, Optional::Some));
     }
 
     Ok(())
 }
 
 fn socket(checksum: &Checksum, path: &Path, metadata: Metadata) -> Result<()> {
-    let sha = begin(path, &metadata, b's');
+    let sha = begin(path, Some(&metadata), b's');
     checksum.put(sha);
 
     Ok(())
 }
 
-fn begin(path: &Path, metadata: &Metadata, kind: u8) -> Sha1 {
+fn begin(path: &Path, metadata: Option<&Metadata>, kind: u8) -> Sha1 {
     let mut sha = Sha1::new();
     let path_bytes = path.as_os_str().as_bytes();
     sha.update([kind]);
     sha.update((path_bytes.len() as u32).to_le_bytes());
     sha.update(path_bytes);
-    sha.update(metadata.mode().to_le_bytes());
+    if let Some(metadata) = metadata {
+        sha.update(metadata.mode().to_le_bytes());
+    }
     sha
 }
 
